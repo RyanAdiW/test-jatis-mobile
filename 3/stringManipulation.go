0031dfe9e48dfa0589cea03a85381c447957aef2
Ryan Adi <muhamad.ryan@sirclo.com>
2023-02-08 12:31:44 +0700

    create function count charr on string

--- 3/stringManipulation.go
+++ 3/stringManipulation.go
@@ -16,11 +16,24 @@ func main() {
 	input = strings.ToLower(input)
 	input = strings.ReplaceAll(input, " ", "")
 
+	// for _, v := range input {
+	// 	if _, ok := mapInput[string(v)]; !ok {
+	// 		countCharr := strings.Count(input, string(v))
+	// 		mapInput[string(v)] = countCharr
+	// 		if strings.Count(input, string(v)) == 1 {
+	// 			outputArr = append(outputArr, string(v))
+	// 		} else {
+	// 			countCharrStr := strconv.Itoa(countCharr)
+	// 			outputArr = append(outputArr, countCharrStr+string(v))
+	// 		}
+	// 	}
+	// }
+
 	for _, v := range input {
 		if _, ok := mapInput[string(v)]; !ok {
-			countCharr := strings.Count(input, string(v))
+			countCharr := countString(input, string(v))
 			mapInput[string(v)] = countCharr
-			if strings.Count(input, string(v)) == 1 {
+			if countString(input, string(v)) == 1 {
 				outputArr = append(outputArr, string(v))
 			} else {
 				countCharrStr := strconv.Itoa(countCharr)
@@ -32,4 +45,15 @@ func main() {
 	output := strings.Join(outputArr, "")
 
 	fmt.Println("output =", output)
+}
+
+// dani
+func countString(str, subStr string) int {
+	count := 0
+	for _, v := range str {
+		if string(v) == subStr {
+			count++
+		}
+	}
+	return count
 }
